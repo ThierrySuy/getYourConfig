a47e474b47b18f8be19884026dc4c49728735234
Titipro <suy.thierry21@gmail.com>
2018-10-18 11:56:38 +0200

    change html to php for carrying <?php>

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue">
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© 2018 Copyright |
+          <a href="http://www.b-acceptance.com/"> B/Acceptance</a>
+        </div>
+        <!-- Copyright -->
+      
+</footer>
+<!-- Footer -->
--- index.php
+++ index.php
@@ -73,16 +73,7 @@
 
 
 </body>
-<!-- Footer -->
-<footer class="page-footer font-small blue">
-
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3">© 2018 Copyright |
-          <a href="http://www.b-acceptance.com/"> B/Acceptance</a>
-        </div>
-        <!-- Copyright -->
-      
-      </footer>
-      <!-- Footer -->
-
+<?php
+require('./footer.html');
+?>
 </html>
